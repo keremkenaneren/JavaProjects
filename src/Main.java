3ac697821939862b41a8bd5c7b90a86bb1fbf444
Kerem Kenan Eren <114931693+ErenKerem82@users.noreply.github.com>
2022-12-24 12:24:51 +0300

    JavaProjects
    Projects

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        /*
+        Km Başı Ücretimiz : 2.20 TL
+        Minimum Tutarımız 20 TL olmakla beraber , 20 TL altındaki ücretlerde 20 TL alınacaktır.
+        Taksimetremizin açılış ücreti 10 TL dir.
+         */
+
+        double ucret, acilisUcreti, indiBindi, kmBasi, gidilenMesafe;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("İndi Bindi Ücretimiz Alınan Karar Gereği 20 Tl Olarak Belirlenmiştir.");
+        System.out.print("GİDİLEN MESAFEMİZ (KM) = ");
+        gidilenMesafe = input.nextDouble();
+
+        acilisUcreti = 10;
+        indiBindi = 20;
+        kmBasi = 2.20;
+        ucret = (gidilenMesafe*kmBasi)+acilisUcreti;
+        ucret = ucret < indiBindi ? 20 : ucret;
+
+        System.out.println("Açılış Ücretimiz = " + acilisUcreti);
+        System.out.println("Kilometre Başı Ücretimiz = " + kmBasi);
+        System.out.println("Ödeyeceğiniz Tutar = " + ucret);
+        
+
+
+    }
+}
